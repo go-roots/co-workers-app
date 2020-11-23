b15c39dd29e6673f48afa26711df98731e0fd849
PaulAmb325 <63868715+PaulAmb325@users.noreply.github.com>
2020-11-23 14:50:35 +0100

    redux events

--- client/user/src/components/events/Events.js
+++ client/user/src/components/events/Events.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect, useCallback } from 'react';
+import React, {Fragment, useEffect, useCallback, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {fetchEvents} from '../../store/actions/events'
 
@@ -17,29 +17,21 @@ const Events = () => {
         fetchData();
     }, []);
 
+    const [currenEvent, setEvent] = useState()
 
     return (
         <section className="container main">
             <div className="row">
                 <div className="col-md-4 col-lg-4 col-xl-4 offset-xl-1 vh-container-small">
                     <ul className="list-group">
-                        <li className="list-group-item">
-                            <div className="d-flex flex-row justify-content-between align-items-start">
-                                <p id="halloween-party-header" style={{ fontFamily: 'Alatsi, sans-serif' }} >Halloween party</p></div>
-                            <p className="text-center" id="halloween-event-text">We welcome all of you ! If you're interested in that event, join us the 30th October 8pm-2am</p>
-                        </li>
-                        <li className="list-group-item">
-                            <p className="text-right" id="cinema-event-header">Cinema night</p>
-                            <p className="text-center">You are a cinema fan ? Come to the Cinema night for an entry fee of 2€, and enjoy a nice movie + appetizers &amp; drinks</p>
-                        </li>
-                        <li className="list-group-item">
-                            <p className="text-center" id="chinese-className-event-header">Chinese className</p>
-                            <p className="text-center">If you're interested in Chinese or Chinese culture, we have free chinese className every Friday afternoon from 4 to 5.30pm</p>
-                        </li>
-                        <li className="list-group-item">
-                            <p className="text-center" id="chinese-className-event-header-1">Speed Chess</p>
-                            <p className="text-center">Speed chess competition to chill out after a hard work day, 3rd November 6pm !</p>
-                        </li>
+                        {events.events.map(event => 
+                            <li className="list-group-item">
+                                <div className="d-flex flex-row justify-content-between align-items-start">
+                                    <p id="halloween-party-header" style={{ fontFamily: 'Alatsi, sans-serif' }} >{event.title}</p></div>
+                                    <p className="text-center" id="halloween-event-text">{event.description}</p>
+                            </li>
+                        )}
+                        
                     </ul>
                 </div>
                 <div className="col-md-8 col-lg-8 col-xl-7 align-self-center responsive-margin"><img className="img-fluid" src={""} alt="pic" /></div>
